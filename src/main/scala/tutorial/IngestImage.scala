c13ba5aa5f890bf4331c79ea312469ca16c0fb10
Eugene Cheipesh <echeipesh@gmail.com>
2017-09-06 14:40:18 -0400

    Reproject scenes before tiling

--- src/main/scala/tutorial/IngestImage.scala
+++ src/main/scala/tutorial/IngestImage.scala
@@ -5,6 +5,7 @@ import geotrellis.raster.io.geotiff._
 import geotrellis.raster.render._
 import geotrellis.raster.resample._
 import geotrellis.raster.reproject._
+import geotrellis.raster.split.Split
 import geotrellis.proj4._
 
 import geotrellis.spark._
@@ -51,37 +52,66 @@ object IngestImage {
 
   def fullPath(path: String) = new java.io.File(path).getAbsolutePath
 
+  /** Calculate the layer metadata for the incoming landsat images
+   *
+   * Normally we would have no information about the incoming rasters and we be forced
+   * to use [[TileLayerMetadata.fromRdd]] to collect it before we could tile the imagery.
+   * But in this case the pre-query from scala-landsatutil is providing enough
+   * information that the metadata can be calculated.
+   *
+   * Collecting metadata before tiling step requires either reading the data twice
+   * or caching records in spark memory. In either case avoiding collection is a performance boost.
+   */
+  def calculateTileLayerMetadata(maxZoom: Int = 13, destCRS: CRS = WebMercator) = {
+    val layoutDefinition = ZoomedLayoutScheme.layoutForZoom(maxZoom, destCRS.worldExtent, 256)
+    // simplify and pretend like we have tiles to cover the world
+    //instead of: val imageExtent = images.map(_.footprint.envelope).reduce(_ combine _).reproject(LatLng, destCRS)
+    val imageExtent = WebMercator.worldExtent
+
+    val GridBounds(colMin, rowMin, colMax, rowMax) = layoutDefinition.mapTransform(imageExtent)
+    TileLayerMetadata(
+      cellType = UShortCellType,
+      layout = layoutDefinition,
+      extent = imageExtent,
+      crs = destCRS,
+      bounds = KeyBounds(
+        SpatialKey(colMin, rowMin),
+        SpatialKey(colMax, rowMax))
+    )
+  }
+
   def run(implicit sc: SparkContext) = {
     // Read the geotiff in as a single image RDD,
     // using a method implicitly added to SparkContext by
     // an implicit class available via the
     // "import geotrellis.spark.io.hadoop._ " statement.
-    val inputRdd: RDD[(ProjectedExtent, MultibandTile)] =
-      sc.hadoopMultibandGeoTiffRDD(inputPath)
+    val reprojected: RDD[(ProjectedExtent, MultibandTile)] =
+      HadoopGeoTiffRDD.spatialMultiband(
+        inputPath, HadoopGeoTiffRDD.Options(maxTileSize=Some(512), numPartitions=Some(8))
+      ).map { case (ProjectedExtent(extent, crs), scene) =>
+          val reprojected = Raster(scene, extent).reproject(crs, WebMercator)
+          ProjectedExtent(reprojected.extent, WebMercator) -> reprojected.tile
+      }
 
-    // Use the "TileLayerMetadata.fromRdd" call to find the zoom
-    // level that the closest match to the resolution of our source image,
-    // and derive information such as the full bounding box and data type.
-    val (_, rasterMetaData) =
-      TileLayerMetadata.fromRdd(inputRdd, FloatingLayoutScheme(512))
+
+    // Avoid use the "TileLayerMetadata.fromRdd" to save an iteration over the files
+    // Since we know our input is landsat we can construct appropriate layer metadata
+    val baseZoom = 13
+    val rasterMetaData = calculateTileLayerMetadata(baseZoom, WebMercator)
 
     // Use the Tiler to cut our tiles into tiles that are index to a floating layout scheme.
     // We'll repartition it so that there are more partitions to work with, since spark
     // likes to work with more, smaller partitions (to a point) over few and large partitions.
     val tiled: RDD[(SpatialKey, MultibandTile)] =
-      inputRdd
+      reprojected
         .tileToLayout(rasterMetaData.cellType, rasterMetaData.layout, Bilinear)
-        .repartition(100)
+
+    val baseLayer: MultibandTileLayerRDD[SpatialKey] = ContextRDD(tiled, rasterMetaData)
 
     // We'll be tiling the images using a zoomed layout scheme
     // in the web mercator format (which fits the slippy map tile specification).
     // We'll be creating 256 x 256 tiles.
     val layoutScheme = ZoomedLayoutScheme(WebMercator, tileSize = 256)
-
-    // We need to reproject the tiles to WebMercator
-    val (zoom, reprojected): (Int, RDD[(SpatialKey, MultibandTile)] with Metadata[TileLayerMetadata[SpatialKey]]) =
-      MultibandTileLayerRDD(tiled, rasterMetaData)
-        .reproject(WebMercator, layoutScheme, Bilinear)
 
     // Create the attributes store that will tell us information about our catalog.
     val attributeStore = FileAttributeStore(outputPath)
@@ -90,7 +120,7 @@ object IngestImage {
     val writer = FileLayerWriter(attributeStore)
 
     // Pyramiding up the zoom levels, write our tiles out to the local file system.
-    Pyramid.upLevels(reprojected, layoutScheme, zoom, Bilinear) { (rdd, z) =>
+    Pyramid.upLevels(baseLayer, layoutScheme, baseZoom, Bilinear) { (rdd, z) =>
       val layerId = LayerId("landsat", z)
       // If the layer exists already, delete it out before writing
       if(attributeStore.layerExists(layerId)) {
